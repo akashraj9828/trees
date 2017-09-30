ffac641f1bb29247100f76bce9b2f0e70410a60e
AKASH RAJ <akashraj9828@gmail.com>
2017-09-30 22:19:15 +0530

    features added [+] searching

--- main.cpp
+++ main.cpp
@@ -19,6 +19,63 @@ public:
 		ptr = root;
 	}
 
+	void search(int data) {
+
+		bool found = false;
+		if (root == NULL) {
+			cout << "\n\n\n\n\t\t\t\t tree is empty :(";
+		}
+		else {
+			Node *parent;
+			ptr = root;
+			parent = ptr;
+			while (ptr!=nullptr) {
+				if (data == ptr->value) {				/////COMPARE given value with 'ptr' node of tree
+					found = true;
+					cout << "\n\nSearch completed :) \n" << data << " found "<<endl;
+					cout << "\n Parent of : " << data << " is " << parent->value;
+					return;
+
+				}
+				else
+					if (data < ptr->value) {			/////if value to be searched is less than current node value then
+					
+					if (ptr->left != nullptr) {
+						parent = ptr;
+						cout << ptr->value<<"->";
+						ptr = ptr->left;				//////set current node to node in its left
+						continue;
+					}
+					else {
+						cout << ptr->value << "->NULL"; 
+						cout << "\n\n\n\n\n\t\t\t\t\t element not found in tree";
+						return;
+					}
+				}
+				else 
+					if(data>ptr->value){		/////if value to be searched is greater than current node value then
+					
+					if (ptr->right != nullptr ) {
+						parent = ptr;
+						cout << ptr->value << "->";
+						ptr = ptr->right;				//////set current node to node in its right
+						continue;
+					}
+					else {
+						cout << ptr->value << "->NULL";
+						cout << "\n\n\n\n\n\t\t\t\t\t element not found in tree";
+						return;
+					}
+					
+				}
+				
+
+			}
+			
+		}
+		
+	}
+
 	void insert(int data) {
 
 		if (root == NULL) {
@@ -28,10 +85,10 @@ public:
 		else {
 			ptr = root;
 			while (true) {
-				if (data<ptr->value) {
+				if (data < ptr->value) {
 					// if the current node already has left child
 					// so we concern it further
-					if ( ptr->left !=NULL ) {
+					if (ptr->left != NULL) {
 						ptr = ptr->left;
 						continue;
 						// if the current node has no left child
@@ -56,8 +113,8 @@ public:
 				}
 			}
 		}
-		}
-	
+	}
+
 
 	void inorder(const Node *root)const {
 		if (root != NULL) {
@@ -68,7 +125,7 @@ public:
 	}
 
 	void preorder(const Node *root)const {
-		
+
 		if (root != NULL) {
 			cout << root->value << '\t';
 			preorder(root->left);
@@ -89,7 +146,7 @@ public:
 	}
 
 	void inorder() const {
-		inorder(root); 
+		inorder(root);
 	};
 };
 
@@ -121,12 +178,12 @@ void printVector(vector<int> v) {
 
 void addNode() {
 	system("cls");
-	cout <<"\n NODE ADDITION MENU ::\n\n" ;
+	cout << "\n NODE ADDITION MENU ::\n\n";
 	cout << "\n ENTER THE NODE's DATA:\t";
 	int data;
 	cin >> data;
 	binary.insert(data);
-	cout << "\n\n\t"<<data<<" succesfully inserted in tree";
+	cout << "\n\n\t" << data << " succesfully inserted in tree";
 	cout << "\n\n\tdo you wish to insert more node?(y/n)\n\n";
 	char c;
 	cin >> c;
@@ -136,6 +193,25 @@ void addNode() {
 	else {
 		menu();
 	}
+}
+void searchNode() {
+	system("cls");
+	cout << "\n NODE SEARCH MENU ::\n\n";
+	cout << "\n ENTER THE NODE's TO BE SEARCHED:\t";
+	int data;
+	cin >> data;
+	binary.search(data);
+	//cout << "\n\n\t" << data << " succesfully inserted in tree";
+	cout << "\n\n\tdo you wish to search another node?(y/n)\n\n";
+	char c;
+	cin >> c;
+	if (c == 'y' || c == 'Y') {
+		searchNode();
+	}
+	else {
+		menu();
+	}
+
 }
 void printNode() {
 	system("cls");
@@ -148,31 +224,49 @@ void printNode() {
 	int order;
 	cin >> order;
 	system("cls");
-	cout << "/n/n/n/n/n";
+	cout << "\n\n\n\n";
 	switch (order) {
 	case 1:
-			cout << endl << "preorder is::" << endl;
-			binary.preorder(binary.root);
-			break;
+		cout << endl << "preorder is::" << endl;
+		binary.preorder(binary.root);
+		getch();
+		printNode();
+		break;
 	case 2:
-			cout << endl << "inorder is::" << endl;
-			binary.inorder(binary.root);
-			break;
+		cout << endl << "inorder is::" << endl;
+		binary.inorder(binary.root);
+		getch();
+		printNode();
+		break;
 	case 3:
-			cout << endl << "postorder is::" << endl;
-			binary.postorder(binary.root);
-			break;
-
+		cout << endl << "postorder is::" << endl;
+		binary.postorder(binary.root);
+		getch();
+		printNode();
+		break;
+	case 4:
+		cout << endl << "inorder is::" << endl;
+		binary.inorder(binary.root);
+		cout << endl << "preorder is::" << endl;
+		binary.preorder(binary.root);
+		cout << endl << "postorder is::" << endl;
+		binary.postorder(binary.root);
+		getch();
+		printNode();
+		break;
 	case 5:
-			menu();
-			break;
+		menu();
+		break;
 	default:
-			cout << endl << "inorder is::" << endl;
-			binary.inorder(binary.root);
-			cout << endl << "preorder is::" << endl;
-			binary.preorder(binary.root);
-			cout << endl << "postorder is::" << endl;
-			binary.postorder(binary.root);
+		cout << endl << "inorder is::" << endl;
+		binary.inorder(binary.root);
+		cout << endl << "preorder is::" << endl;
+		binary.preorder(binary.root);
+		cout << endl << "postorder is::" << endl;
+		binary.postorder(binary.root);
+		getch();
+		printNode();
+		break;
 
 
 	}
@@ -192,9 +286,12 @@ void operation(int choice) {
 		printNode();
 		break;
 	case 4:
+		searchNode();
+		break;
+	case 5:
 		cout << "\n\n\n\n\n\t\t\t\t\t\t\t PRESS ANY KEY TO EXIT !";
 		getch();
-		return ;
+		return;
 		break;
 	default:
 		cout << "\n\n\n\n\t\t\t\t\t\t *_* *_* *_* *_* *_*";
@@ -207,7 +304,7 @@ void operation(int choice) {
 	}
 }
 int menu() {
-	
+
 	system("cls");
 	COORD xy = getsize();
 	//gotoxy(xy.X / 2 - 10, xy.Y / 3);
@@ -216,7 +313,8 @@ int menu() {
 	cout << "\t 1. add node" << endl;
 	cout << "\t 2. delete node" << endl;
 	cout << "\t 3. show nodes" << endl;
-	cout << "\t 4. exit" << endl;
+	cout << "\t 4. search element" << endl;
+	cout << "\t 5. exit" << endl;
 	int choice;
 	cin >> choice;
 	operation(choice);
@@ -225,45 +323,35 @@ int menu() {
 
 
 int main() {
-	//vector<int> values;
-	//values= { 25,15,10,4,12,22,18,24,50,70,66,90,35,31,44 }; //example 1
-	//values = {43,15,8,30,20,35,60,50,82,70};  //example 2
-	//
-	//for (int i = 0; i < values.size(); i++) {
-	//	
-	//	binary.insert(values[i]);
-	//	//cout <<sizeof(v)<<"::"<< v[i]<<endl;
-	//}
-	menu();
-//	cout << endl << "entered data::"<<endl;
-//	printVector(values);
-//	cout << endl << "inorder is::" << endl;
-//	binary.inorder(binary.root);
-//	cout << endl << "preorder is::" << endl;
-//	binary.preorder(binary.root);
-//	cout << endl << "postorder is::" << endl;
-//	binary.postorder(binary.root);
-	getch();
+
+	bool debugging = false;
+	if (!debugging) {
+		menu();
+	}
+	else {
+		vector<int> values;
+		values= { 25,15,10,4,12,22,18,24,50,70,66,90,35,31,44 }; //example 1 image included in source
+		//values = { 43,15,8,30,20,35,60,50,82,70 };  //example 2  image included in source
+
+		for (int i = 0; i < values.size(); i++) {
+
+			binary.insert(values[i]);
+			//cout <<sizeof(v)<<"::"<< v[i]<<endl;
+		}
+
+		cout << endl << "entered data::" << endl;
+		printVector(values);
+		cout << endl << "inorder is::" << endl;
+		binary.inorder(binary.root);
+		cout << endl << "preorder is::" << endl;
+		binary.preorder(binary.root);
+		cout << endl << "postorder is::" << endl;
+		binary.postorder(binary.root);
+		menu();
+		getch();
+	}
 
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
